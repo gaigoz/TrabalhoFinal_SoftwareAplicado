500444b73d5099638a6eba044eb9c895d03c8cad
jppmedeiros <80531244+jppmedeiros@users.noreply.github.com>
2022-11-28 15:47:30 -0300

    Update Produto.cs

--- Entidades/Model/Produto.cs
+++ Entidades/Model/Produto.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -29,7 +29,6 @@ namespace Entidades.Model
         public int CategoriaId { get; set; }
         public Categoria Categoria { get; set; }
 
-        public int FaqId { get; set; }
-        public Faq Faq { get; set; }
+        public List<Faq> Faq { get; set; }
     }
 }
